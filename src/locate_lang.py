de903f26f9d02dac8c174e0d275e78a48aa00c3b
IvoFelix <ivo.felix@sapo.pt>
2021-12-16 14:38:49 +0000

    Created LocateLang as well as some general tests and output code

---
+++ src/locate_lang.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+from typing import Dict, List, Tuple
+
+from fcm import Fcm
+
+
+class LocateLang:
+    fcms: Tuple[Fcm, ...]
+    fcms_results: Dict[str, List[float]]
+    # fcm_indexes: Dict[str, List[int]]
+    fcm_indexes: Dict[str, List[Tuple[int, int]]]
+    threshold: int
+
+    # TODO: instead of receiving the Fcm instances receive the paths and cache
+    # if necessary
+    def __init__(self, threshold, *fcms: Fcm) -> None:
+        self.threshold = threshold
+        self.fcms = fcms
+        self.fcms_results = defaultdict(list)
+        self.fcm_indexes = defaultdict(list)
+
+    def generate_fcm_results(self, target_path: str):
+        context_sizes = [fcm.context_size for fcm in self.fcms]
+        max_context_size = max(context_sizes)
+
+        with open(target_path) as target_file:
+            target_text = target_file.read()
+
+            for i in range(max_context_size + 1, len(target_text)):
+                context = target_text[i - max_context_size:i]
+                for j, fcm in enumerate(self.fcms, start=1):
+                    # TODO: this needs to change according to the
+                    # reference file's name
+                    fcm_name = f'fcm{j}'
+                    self.fcms_results[fcm_name].append(
+                        fcm.get_information_amount(
+                            target_text[i], context[-fcm.context_size:]))
+
+    def find_fcm_indexes(self):
+        fcm_name_list = list(self.fcms_results.keys())
+        nr_chars = len(self.fcms_results['fcm1'])
+        best_count = 0
+        current_best_fcm = fcm_name_list[0]
+
+        segment_start = 0
+        current_segment_fcm = ''
+        for i in range(0, nr_chars):
+            previous_best_fcm = current_best_fcm
+
+            # find the fcm with the minimum information for this char
+            current_best_fcm = fcm_name_list[0]
+            minimum = self.fcms_results[current_best_fcm][i]
+            for fcm in self.fcms_results:
+                if self.fcms_results[fcm][i] < minimum:
+                    current_best_fcm = fcm
+                    minimum = self.fcms_results[current_best_fcm][i]
+
+            # if an fcm is the best for the duration of the threshold then
+            # start a new segment where that fcm is the best
+            if current_best_fcm == previous_best_fcm:
+                best_count += 1
+                if best_count == self.threshold and \
+                        current_best_fcm != current_segment_fcm:
+                    if current_segment_fcm != '':
+                        self.fcm_indexes[current_segment_fcm].append(
+                            (segment_start, i - self.threshold))
+                        segment_start = i - self.threshold
+                    current_segment_fcm = current_best_fcm
+            else:
+                best_count = 0
+
+        self.fcm_indexes[current_best_fcm].append(
+            (segment_start, nr_chars - 1))
---
+++ src/locate_lang_test.py
@@ -0,0 +1,43 @@
+from matplotlib import pyplot
+
+from fcm import Fcm
+from locate_lang import LocateLang
+
+context_size = 3
+fcm1 = Fcm(0.2, context_size)
+fcm1.add_file('datasets/por_PT.latn.Portugese.EP7-train.utf8')
+fcm2 = Fcm(0.2, context_size)
+fcm2.add_file('datasets/eng_GB.latn.English.EP7-train.utf8')
+
+locate_lang_target = 'examples/locatelangtest.txt'
+locate_lang = LocateLang(3, fcm1, fcm2)
+locate_lang.generate_fcm_results(locate_lang_target)
+
+results = locate_lang.fcms_results
+locate_lang.find_fcm_indexes()
+indexes = locate_lang.fcm_indexes
+
+pyplot.plot([i for i in range(context_size, len(results['fcm1']) +
+            context_size)], results['fcm1'], label='Fcm1', color='blue')
+pyplot.plot([i for i in range(context_size, len(results['fcm2']) +
+            context_size)], results['fcm2'], label='Fcm2', color='green')
+pyplot.xlabel('Char index')
+pyplot.ylabel('Information amount')
+pyplot.legend()
+
+print(indexes)
+for fcm in indexes:
+    if fcm == 'fcm1':
+        color = 'blue'
+    else:
+        color = 'green'
+    for span in indexes[fcm]:
+        pyplot.axvspan(span[0], span[1], alpha=0.2, color=color)
+
+with open(locate_lang_target) as target_file:
+    target_text = target_file.read()
+    for i, char in enumerate(target_text):
+        pyplot.annotate(char, (i, -0.5), horizontalalignment='center',
+                        fontfamily='monospace', fontsize='x-small')
+
+pyplot.show()
