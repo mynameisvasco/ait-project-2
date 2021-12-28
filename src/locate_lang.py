fdc7cd9966e00862adcc49630b0af3fb3857ea64
IvoFelix <ivo.felix@sapo.pt>
2021-12-28 01:16:02 +0000

    Use Lang and Target classes within LocateLang

--- src/locate_lang.py
+++ src/locate_lang.py
@@ -1,68 +1,73 @@
 from collections import defaultdict
+from pathlib import Path
 from typing import Dict, List, Tuple
 
-from fcm import Fcm
+from lang import Lang
+from target import Target
+from time import perf_counter
 
 
 class LocateLang:
-    fcms: Tuple[Fcm, ...]
-    fcms_results: Dict[str, List[float]]
-    # fcm_indexes: Dict[str, List[int]]
-    fcm_indexes: Dict[str, List[Tuple[int, int]]]
+    lang_results: Dict[str, List[float]]
+    lang_segments: Dict[str, List[Tuple[int, int]]]
+    reference_chars: int
     threshold: int
 
-    # TODO: instead of receiving the Fcm instances receive the paths and cache
-    # if necessary
-    def __init__(self, threshold, *fcms: Fcm) -> None:
+    def __init__(self, references_path: str, target_path: str,
+                 reference_chars: int, threshold: int) -> None:
         self.threshold = threshold
-        self.fcms = fcms
-        self.fcms_results = defaultdict(list)
-        self.fcm_indexes = defaultdict(list)
+        self.lang_results = defaultdict(list)
+        self.lang_segments = defaultdict(list)
+        self.reference_chars = reference_chars
+        reference_paths = list(Path(references_path).rglob("*.utf8"))
+        langs = []
 
-    def generate_fcm_results(self, target_path: str):
-        context_sizes = [fcm.context_size for fcm in self.fcms]
-        max_context_size = max(context_sizes)
+        print("Building references...")
+        start = perf_counter()
 
-        with open(target_path) as target_file:
+        for reference_path in reference_paths:
+            lang = Lang(str(reference_path), reference_chars)
+            langs.append(lang)
+
+        perf = (perf_counter() - start)
+        print(f"Built all references after {perf}s")
+
+        with open(target_path, "r") as target_file:
             target_text = target_file.read()
+            target = Target(target_text, 5, None)
 
-            for i in range(max_context_size, len(target_text)):
-                context = target_text[i - max_context_size:i]
-                for j, fcm in enumerate(self.fcms, start=1):
-                    # TODO: this needs to change according to the
-                    # reference file's name
-                    fcm_name = f'fcm{j}'
-                    self.fcms_results[fcm_name].append(
-                        fcm.get_information_amount(
-                            target_text[i], context[-fcm.context_size:]))
+            for context, symbol in target.generator():
+                for lang in langs:
+                    bits = lang.estimate_bits(context, symbol)
+                    self.lang_results[lang.name].append(bits)
 
-    def find_fcm_indexes(self):
-        k = 3
-        fcm_name_list = list(self.fcms_results.keys())
-        nr_chars = len(self.fcms_results['fcm1'])
+    def find_lang_indexes(self):
+        k = 5
+        lang_name_list = list(self.lang_results)
+        results_length = len(self.lang_results[lang_name_list[0]])
         best_count = 0
-        current_best_fcm = fcm_name_list[0]
-
+        current_best_lang = lang_name_list[0]
         segment_start = 0
         current_segment_fcm = ''
-        for i in range(0, nr_chars):
-            previous_best_fcm = current_best_fcm
-            current_best_fcm = min(self.fcms_results,
-                                   key=lambda x: self.fcms_results[x][i])
+
+        for i in range(0, results_length):
+            previous_best_fcm = current_best_lang
+            current_best_lang = min(self.lang_results,
+                                    key=lambda x: self.lang_results[x][i])
 
             # if an fcm is the best for the duration of the threshold then
             # start a new segment where that fcm is considered the best
-            if current_best_fcm == previous_best_fcm:
+            if current_best_lang == previous_best_fcm:
                 best_count += 1
                 if best_count == self.threshold and \
-                        current_best_fcm != current_segment_fcm:
+                        current_best_lang != current_segment_fcm:
                     if current_segment_fcm != '':
-                        self.fcm_indexes[current_segment_fcm].append(
+                        self.lang_segments[current_segment_fcm].append(
                             (segment_start, i - self.threshold))
                         segment_start = i - self.threshold
-                    current_segment_fcm = current_best_fcm
+                    current_segment_fcm = current_best_lang
             else:
                 best_count = 0
 
-        self.fcm_indexes[current_best_fcm].append(
-            (segment_start, nr_chars + k - 1))
+        self.lang_segments[current_best_lang].append(
+            (segment_start, results_length + k - 1))
